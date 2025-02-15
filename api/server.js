f67017edf92e5e9124d18ffbfc4b43884a447198
Tassio Neves <tassions@hotmail.com>
2025-02-15 14:40:20 -0300

    fix: atribut duration

--- api/server.js
+++ api/server.js
@@ -2,8 +2,12 @@ import express from "express";
 import { db } from "./connect.js";
 import { artistArray } from "../database/artists.js";
 import { songsArray } from "../database/songs.js";
+import cors from "cors";
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
+//app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("Olá, Mundo!");
@@ -13,8 +17,8 @@ app.get("/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", (request, response) => {
-  response.send(songsArray);
+app.get("/songs", async (request, response) => {
+  response.send(await db.collection("songs").find({}).toArray());
 });
 app.listen(PORT, () => {
   console.log(`Servidor está escutando na porta ${PORT}`);
